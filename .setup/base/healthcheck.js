6bb006c602f9dd9a2f65a64db8f6d1ba5ab68d48
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-28 21:49:59 -0300

    chore(setup): :wrench: remove commented code for clarity

--- .setup/base/healthcheck.js
+++ .setup/base/healthcheck.js
@@ -13,21 +13,18 @@ var options = {
 
 if (settings.hasOwnProperty("https")) {
     request = https.request(options, (res) => {
-        //console.log(`STATUS: ${res.statusCode}`);
         if ((res.statusCode >= 200) && (res.statusCode < 500)) { process.exit(0); }
         else { process.exit(1); }
     });
 }
 else {
     request = http.request(options, (res) => {
-        //console.log(`STATUS: ${res.statusCode}`);
         if ((res.statusCode >= 200) && (res.statusCode < 500)) { process.exit(0); }
         else { process.exit(1); }
     });
 }
 
 request.on('error', function(err) {
-    //console.log('ERROR',err);
     process.exit(1);
 });
 
